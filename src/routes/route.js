3bd8155851176e8792bada5d46a701087470ba39
maldosftorres <fermaldos@hotmail.com>
2021-08-26 04:33:08 -0400

    tercer commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,10 +5,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('index.html', { title: 'FM' });
 });
-//Blog Page
-router.get('/blog', (req, res) => {
-    res.render('blog.html', { title: 'FM' });
-});
+
 //Contact Page
 router.get('/contact', (req, res) => {
     res.render('contact.html', { title: 'FM' });
